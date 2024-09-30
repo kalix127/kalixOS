921d973913d1579a78a9a2290a4593e014d24c62
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-10-01 00:54:26 +0200

    chore: Setup shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "shadcn-nuxt"],
   css: ["~/assets/css/main.css"],
   icon: {
     serverBundle: {
@@ -11,5 +11,16 @@ export default defineNuxtConfig({
     clientBundle: {
       scan: true,
     },
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
   },
 });

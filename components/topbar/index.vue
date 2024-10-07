1fdd5bdbbd4150ca51c2c24fd057392849b6e6e3
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-10-07 19:46:23 +0200

    feat: Add topbar

---
+++ components/topbar/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const route = useRoute();
+</script>
+
+<template>
+  <header
+    class="flex items-center justify-between bg-[#080404] px-1"
+    :class="[route.name === 'login' ? '!bg-transparent' : '']"
+  >
+    <!-- Left empty div for spacing -->
+    <div class="hidden sm:block sm:w-1/3"></div>
+
+    <!-- Calendar -->
+    <TopbarCalendar />
+
+    <!-- Main menu -->
+    <div class="flex w-1/3 items-center justify-end">
+      <TopbarActions />
+    </div>
+  </header>
+</template>

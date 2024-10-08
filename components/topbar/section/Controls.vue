a15de1b938ce15a6f3b3f0ae5ece0bd7b4a6797a
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-10-09 00:10:40 +0200

    refactor: Improve volume slider in topbar Controls component

--- components/topbar/section/Controls.vue
+++ components/topbar/section/Controls.vue
@@ -10,27 +10,34 @@ const { volume, isPowerOffMenuOpen, isAnyTopbarMenuOpen } =
 
 <template>
   <div
-    class="topbar-menu-transition mb-4 mt-2 flex min-h-8 items-center gap-4"
+    class="topbar-menu-transition mb-4 mt-2 space-y-2"
     :class="[isAnyTopbarMenuOpen ? 'brightness-75' : '']"
   >
-    <Icon v-show="volume[0] > 50" name="material-symbols:volume-up" size="24" />
-    <Icon
-      v-show="volume[0] > 0 && volume[0] <= 50"
-      name="material-symbols:volume-down"
-      size="24"
-    />
-    <Icon
-      v-show="volume[0] === 0"
-      name="material-symbols:volume-off"
-      size="24"
-    />
-    <Slider
-      v-model="volume"
-      :disabled="isAnyTopbarMenuOpen"
-      :default-value="volume"
-      :max="100"
-      :step="1"
-    />
+    <!-- Volume slider -->
+    <div class="flex min-h-8 items-center gap-4">
+      <Icon
+        v-show="volume[0] > 50"
+        name="material-symbols:volume-up"
+        size="20"
+      />
+      <Icon
+        v-show="volume[0] > 0 && volume[0] <= 50"
+        name="material-symbols:volume-down"
+        size="20"
+      />
+      <Icon
+        v-show="volume[0] === 0"
+        name="material-symbols:volume-off"
+        size="20"
+      />
+      <Slider
+        v-model="volume"
+        :disabled="isAnyTopbarMenuOpen"
+        :default-value="volume"
+        :max="100"
+        :step="1"
+      />
+    </div>
   </div>
 </template>
 

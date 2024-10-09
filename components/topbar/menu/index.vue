266e54e29d0ff53a38c5cdfc7500e48de289192f
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-10-09 18:06:33 +0200

    style: Update icon size and button style in topbar menu

--- components/topbar/menu/index.vue
+++ components/topbar/menu/index.vue
@@ -54,12 +54,12 @@ const isEnabled = computed(() => {
       <div class="mt-4 select-none space-y-4 rounded-2xl bg-secondary p-4">
         <div class="flex items-center gap-4">
           <div
-            class="grid place-content-center rounded-full p-3"
+            class="grid place-content-center rounded-full p-2"
             :class="[isEnabled ? 'bg-primary' : 'bg-accent']"
           >
-            <Icon :name="icon" size="24" />
+            <Icon :name="icon" size="26" />
           </div>
-          <span class="text-xl font-bold">
+          <span class="text-xl font-extrabold">
             {{ title }}
           </span>
         </div>

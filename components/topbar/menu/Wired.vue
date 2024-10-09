155277bb711c9e71b4f71bb886126c66de98bbfc
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-10-09 04:08:20 +0200

    style: style: Update Wired menu button style

--- components/topbar/menu/Wired.vue
+++ components/topbar/menu/Wired.vue
@@ -20,13 +20,13 @@ function closeMenu() {
   >
     <Button
       variant="ghost"
-      class="flex w-full cursor-default items-center justify-between rounded-xl font-medium duration-0 last:mt-2 last:rounded-none last:border-t last:border-t-accent hover:bg-accent-light last:hover:rounded-xl"
+      class="flex w-full cursor-default items-center justify-between rounded-xl font-medium duration-0 hover:bg-accent"
     >
       <div class="flex items-center gap-2">
         <Icon name="lucide:ethernet-port" size="18" />
         <span>Wired</span>
       </div>
-      <span class="text-gray-400"> Disconnect </span>
+      <span class="text-muted-foreground"> Disconnect </span>
     </Button>
   </TopbarMenu>
 </template>

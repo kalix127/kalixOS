54e3dfdb1c9ebbbff5a7092b8432259c266b06d9
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-10-10 05:02:23 +0200

    refactor: Improve validation error messages

--- validations/auth.ts
+++ validations/auth.ts
@@ -3,17 +3,17 @@ import { toTypedSchema } from "@vee-validate/zod";
 
 export const loginValidationSchema = toTypedSchema(
   z.object({
-    password: z.string({ message: "The password is required" }),
+    password: z.string({ message: "The password is required." }),
   }),
 );
 
 export const addUserValidationSchema = toTypedSchema(
   z.object({
     username: z
-      .string({ message: "The username is required" })
+      .string({ message: "The username is required." })
       .min(3, {
-        message: "The username must be at least 3 characters",
+        message: "The username must be at least 3 characters.",
       })
-      .max(16, { message: "The username must be at most 16 characters" }),
+      .max(16, { message: "The username must be at most 16 characters." }),
   }),
 );

eec79331888b6d603ae13dfc1dc5703e28f3ca2d
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-10-12 17:49:02 +0200

    remove: Remove useless text in the blank page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="bg-black h-[100vh]">OFF 100%</main>
+  <main class="bg-black h-[100vh]"></main>
 </template>
 
 <style scoped></style>

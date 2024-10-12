478374e0469317820f67d1e23126093e1f251c20
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-10-12 13:20:05 +0200

    refactor: Refactored login middleware for simplified authentication checks

--- middleware/login.ts
+++ middleware/login.ts
@@ -5,12 +5,9 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const { isAuthenticated } = storeToRefs(useGlobalStore());
 
   // If the user is not authenticated, redirect to the login page
-  if (to.path !== "/login" && !isAuthenticated.value) {
+  if (!isAuthenticated.value) {
     return navigateTo("/login");
   }
-
-  // If the user is authenticated, redirect to the home page
-  if (to.path === "/login" && isAuthenticated.value) {
-    return navigateTo("/");
-  }
 });
+
+

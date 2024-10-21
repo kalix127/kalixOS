0ee14c39f4156ed0d9778b4ab705ff3fe9970e1c
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-10-21 17:10:13 +0200

    refactor: Minor change

--- constants/index.ts
+++ constants/index.ts
@@ -3,14 +3,9 @@ import type { WifiNetwork, FileSystemNode } from "@/types";
 import { assignIds } from "~/helpers";
 
 export const defaultNetworks: WifiNetwork[] = [
-  { id: 1, name: "TIM-7632798", signal: 4, isProtected: true },
-  { id: 2, name: "FASTWEB-1234567", signal: 4, isProtected: true },
-  { id: 3, name: "TIM-34781 Public", signal: 3, isProtected: false },
-  { id: 4, name: "FASTWEB-9876543", signal: 3, isProtected: true },
-  { id: 5, name: "VODAFONE-72624", signal: 3, isProtected: true },
-  { id: 6, name: "TIM-27834", signal: 2, isProtected: true },
-  { id: 7, name: "VODAFONE Public", signal: 2, isProtected: false },
-  { id: 8, name: "ILIAD-528937", signal: 1, isProtected: true },
+  { id: 1, name: "FASTWEB-7632798", signal: 4, isProtected: true },
+  { id: 2, name: "TIM-1234567", signal: 3, isProtected: true },
+  { id: 3, name: "ILIAD-34781 Public", signal: 1, isProtected: false },
 ];
 
 export const defaultFileSystem = (username: string): FileSystemNode =>

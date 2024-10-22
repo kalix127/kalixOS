87a375860263c7f9ab9a1bde75d246a381eb402a
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-10-22 19:21:39 +0200

    refactor: Add handler to toggle System Logs with the 'ESC' keystroke.

--- pages/booting.vue
+++ pages/booting.vue
@@ -1,14 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useMagicKeys } from "@vueuse/core";
+
+definePageMeta({
+  pageTransition: false,
+})
+
+const areSystemLogsVisible = ref(false);
+
+const { escape } = useMagicKeys();
+
+watch(escape, (v) => {
+  if (v) {
+    areSystemLogsVisible.value = !areSystemLogsVisible.value;
+  }
+});
+</script>
 
 <template>
-  <div class="grid min-h-[100vh] grid-rows-[90%_10%] bg-black/50">
+  <div class="grid min-h-[100vh] grid-rows-[90%_10%] bg-black relative">
     <div class="flex flex-col items-center justify-center">
       <LogoAsusRog class="size-48" />
       <Icon name="eos-icons:three-dots-loading" size="80" />
     </div>
+
     <div class="grid place-content-center">
       <LogoManjaroFull class="max-w-48" />
     </div>
+
+    <!-- System Logs on escape -->
+    <OverlaySystemLogs v-show="areSystemLogsVisible"  />
   </div>
 </template>
 

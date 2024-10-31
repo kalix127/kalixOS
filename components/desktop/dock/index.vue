43250f91ba18a09e669001c2aedf81eec208857f
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-10-31 05:06:55 +0100

    refactor: Improved dockbar visiblity toggle's logic

--- components/desktop/dock/index.vue
+++ components/desktop/dock/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useSwipe, watchDebounced } from "@vueuse/core";
+import { useSwipe, watchDebounced, useDebounceFn } from "@vueuse/core";
 
 const desktopStore = useDesktopStore();
 const { isDockVisible, isDockPinned } = storeToRefs(desktopStore);
@@ -21,44 +21,38 @@ const isHovered = computed(
   () => isTriggerHovered.value || isContentHovered.value,
 );
 
-function toggleVisibility(value: boolean) {
+const setVisibility = useDebounceFn((value: boolean) => {
   if (isDockPinned.value) return;
   isDockVisible.value = value;
-}
+}, 100);
 
 watchDebounced(
   isHovered,
   (newVal) => {
     isDockVisible.value = newVal;
   },
-  { debounce: 50 },
+  { debounce: 100 },
 );
 </script>
 
 <template>
   <TooltipProvider :delay-duration="0">
     <Tooltip :open="isDockVisible" :default-open="isDockVisible">
-      <div
-        class="relative"
-        @mouseenter="() => toggleVisibility(true)"
-        @mouseleave="() => toggleVisibility(false)"
-      >
+      <div class="relative">
         <TooltipTrigger
           ref="dockTriggerRef"
           class="absolute bottom-0 left-0 z-[50000] h-10 w-full cursor-default sm:h-6"
-          @mouseenter="() => toggleVisibility(true)"
-          @mouseleave="() => toggleVisibility(false)"
+          @mouseenter="() => setVisibility(true)"
+          @mouseleave="() => setVisibility(false)"
         >
         </TooltipTrigger>
         <TooltipContent
           :side-offset="-12"
           class="z-[50000] rounded-3xl p-0"
-          @mouseenter="() => toggleVisibility(true)"
-          @mouseleave="() => toggleVisibility(false)"
+          @mouseenter="() => setVisibility(true)"
+          @mouseleave="() => setVisibility(false)"
         >
-          <DesktopDockContent
-            @close="() => toggleVisibility(false)"
-          />
+          <DesktopDockContent @close="() => setVisibility(false)" />
         </TooltipContent>
       </div>
     </Tooltip>

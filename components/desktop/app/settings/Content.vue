a943699b8d62a557966a02ce480e83a563f912ee
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-11-08 23:57:41 +0100

    feat: Add back button for the settings tab on mobile

--- components/desktop/app/settings/Content.vue
+++ components/desktop/app/settings/Content.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { setSettingsTab } = useGlobalStore();
+</script>
 
 <template>
-  <ScrollArea class="h-full w-full px-6 py-8 sm:px-12">
+  <ScrollArea class="relative h-full w-full px-6 py-8 sm:px-12">
+    <Button
+      variant="ghost"
+      size="icon"
+      class="absolute left-1.5 top-1.5 grid size-6 place-content-center sm:hidden"
+      @click="() => setSettingsTab(null)"
+    >
+      <Icon name="gnome:arrow-long-left" size="16" />
+    </Button>
     <slot />
   </ScrollArea>
 </template>

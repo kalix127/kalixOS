4b3c8a631700f3c088b7e5f740a8dc12bd25acbd
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-11-08 22:54:21 +0100

    refactor: Refactored wifi icon component

--- components/topbar/menu/Wifi.vue
+++ components/topbar/menu/Wifi.vue
@@ -3,73 +3,17 @@ import { useTimeoutFn } from "@vueuse/core";
 import { vOnClickOutside } from "@vueuse/components";
 import type { WifiNetwork } from "@/types";
 
-const globalStore = useGlobalStore();
+const { isWifiMenuOpen, isWifiEnabled, isAirplaneModeEnabled } =
+  storeToRefs(useGlobalStore());
+
 const {
-  isWifiEnabled,
-  isAirplaneModeEnabled,
   isConnectingToWifi,
-  isWifiMenuOpen,
   availableWifiNetworks,
   connectedWifiNetwork,
   isSearchingWifiNetworks,
-} = storeToRefs(globalStore);
-
-const idConnectingNetwork = ref<number | null>(null);
-const currentNetwork = ref<WifiNetwork | null>(null);
-
-/**
- * Handles the completion of the WiFi connection process.
- * This function is called after the timeout expires.
- */
-function handleConnectionCompletion() {
-  if (!currentNetwork.value) return;
-
-  // Check if WiFi is still enabled and airplane mode is not active
-  if (!isWifiEnabled.value || isAirplaneModeEnabled.value) {
-    resetConnectionState();
-    return;
-  }
-
-  // Ensure that the network being connected to hasn't changed during the timeout
-  if (idConnectingNetwork.value !== currentNetwork.value.id) {
-    return;
-  }
-
-  // Update the connected network and reset the connection state
-  connectedWifiNetwork.value = currentNetwork.value;
-  resetConnectionState();
-}
-
-function resetConnectionState() {
-  isConnectingToWifi.value = false;
-  idConnectingNetwork.value = null;
-  currentNetwork.value = null;
-}
-
-const { start, stop } = useTimeoutFn(handleConnectionCompletion, 3000, {
-  immediate: false,
-});
-
-function connectToWifi(network: WifiNetwork) {
-  // If the selected network is already connected, disconnect from it
-  if (connectedWifiNetwork.value?.id === network.id) {
-    connectedWifiNetwork.value = null;
-    return;
-  }
-
-  // If a connection attempt is already in progress, cancel it
-  if (isConnectingToWifi.value) {
-    stop();
-  }
-
-  // Update the connection state to indicate that a connection attempt is in progress
-  isConnectingToWifi.value = true;
-  idConnectingNetwork.value = network.id;
-  currentNetwork.value = network;
-
-  // Start the timeout to simulate the connection delay
-  start();
-}
+  idConnectingNetwork,
+  connectToWifi,
+} = useWifi();
 
 function closeMenu() {
   isWifiMenuOpen.value = false;
@@ -99,10 +43,7 @@ function closeMenu() {
       class="flex w-full cursor-pointer justify-start gap-2 rounded-xl font-medium transition-colors duration-200 hover:bg-accent"
       @click="() => connectToWifi(network)"
     >
-      <Icon
-        :name="`gnome:wifi-${network.signal}${network.isProtected ? '-lock' : ''}`"
-        size="18"
-      />
+      <WifiIcon :network="network" :size="18" />
       <span>{{ network.name }}</span>
 
       <Icon
@@ -120,6 +61,4 @@ function closeMenu() {
   </TopbarMenu>
 </template>
 
-<style scoped>
-/* Add any component-specific styles here */
-</style>
+<style scoped></style>

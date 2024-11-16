de887dc6ed153d4f7a3dd7c1957035316d9a4e7d
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-11-17 00:01:19 +0100

    refactor: Hide the dockbar on mobile if there is at least 1 app open

--- components/desktop/dock/index.vue
+++ components/desktop/dock/index.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useSwipe, watchDebounced, useDebounceFn } from "@vueuse/core";
-
+import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
 const desktopStore = useDesktopStore();
-const { isDockVisible, isDockPinned } = storeToRefs(desktopStore);
+const { isDockVisible, isDockPinned, openApps } = storeToRefs(desktopStore);
 
 const dockTriggerRef = ref<HTMLElement | null>(null);
 
@@ -16,6 +16,7 @@ watch(direction, (newVal) => {
 
 const isTriggerHovered = ref(false);
 const isContentHovered = ref(false);
+const isMobile = useBreakpoints(breakpointsTailwind).smaller("sm");
 
 const isHovered = computed(
   () => isTriggerHovered.value || isContentHovered.value,
@@ -41,12 +42,13 @@ watchDebounced(
       <div class="relative">
         <TooltipTrigger
           ref="dockTriggerRef"
-          class="absolute bottom-0 left-0 z-[50000] h-10 w-full sm:h-6 cursor-default"
+          class="absolute bottom-0 left-0 z-[50000] h-10 w-full cursor-default sm:h-6"
           @mouseenter="() => setVisibility(true)"
           @mouseleave="() => setVisibility(false)"
         >
         </TooltipTrigger>
         <TooltipContent
+          v-if="!isMobile || (isMobile && openApps.length === 0)"
           :side-offset="-12"
           class="z-[50000] rounded-3xl p-0"
           @mouseenter="() => setVisibility(true)"

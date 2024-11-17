40bd145e22d6a0d87bb9a0ec4f8620c4de67e073
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-11-17 18:11:48 +0100

    feat: Add launch app animation

--- components/desktop/dock/Item.vue
+++ components/desktop/dock/Item.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { type AppNode } from "@/types";
 
+const showLaunchIcon = ref(false);
+
 const { hasAppsLoading } = storeToRefs(useDesktopStore());
 
-defineEmits<{
+const emit = defineEmits<{
   (e: "openApp", app: AppNode): void;
   (e: "context", event: MouseEvent, app: AppNode): void;
 }>();
@@ -12,19 +14,38 @@ const props = defineProps<{
   app: AppNode;
 }>();
 const { app } = toRefs(props);
+
+function handleClick() {
+  emit("openApp", app.value);
+
+  if (app.value.isOpen) return;
+
+  showLaunchIcon.value = true;
+  setTimeout(() => {
+    showLaunchIcon.value = false;
+  }, 250);
+}
 </script>
 
 <template>
   <DesktopDockItemTooltip :app-name="app.name">
     <Button
       @contextmenu.prevent="(event) => $emit('context', event, app)"
-      @click="() => $emit('openApp', app)"
+      @click="handleClick"
       variant="ghost"
       size="icon"
-      class="relative grid place-content-center rounded-2xl p-6 duration-0 hover:bg-accent/70 sm:p-7"
+      class="group relative grid place-content-center rounded-2xl p-6 duration-0 hover:bg-accent/70 sm:p-7"
       :class="[hasAppsLoading ? 'cursor-progress' : '']"
     >
       <Icon :name="app.icon" class="size-9 sm:size-10" />
+      <Transition name="launch" mode="in-out">
+        <Icon
+          v-show="showLaunchIcon"
+          :name="app.icon"
+          class="size-9 sm:size-10 absolute -translate-y-1/2 -translate-x-1/2 prevent-flicker"
+        />
+      </Transition>
+
       <div
         v-if="app.isOpen"
         class="absolute bottom-0.5 left-1/2 h-1.5 w-1.5 -translate-x-1/2 rounded-full bg-white"
@@ -33,4 +54,31 @@ const { app } = toRefs(props);
   </DesktopDockItemTooltip>
 </template>
 
-<style scoped></style>
+<style scoped>
+.launch-enter-active {
+  animation: launch 0.3s ease;
+}
+
+@keyframes launch {
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(2.5);
+    opacity: 0;
+  }
+}
+
+.launch-enter-from,
+.launch-leave-to {
+  opacity: 0;
+  transform: scale(1);
+}
+
+/* Utility class to prevent flicker */
+.prevent-flicker {
+  backface-visibility: hidden;
+  transform: translateZ(0);
+}
+</style>

8ef58ad086d2dc76fb6236fec9290285d31b9070
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-11-17 18:46:00 +0100

    feat: Add settings default Tab

---
+++ components/desktop/app/settings/TabDefault.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { AppNode } from "@/types";
+
+const props = defineProps<{
+  app: AppNode;
+}>();
+
+const { app } = toRefs(props);
+
+const globalStore = useGlobalStore();
+const { isBluetoothEnabled } = storeToRefs(globalStore);
+</script>
+
+<template>
+  <SettingsContent :app="app">
+    <div class="h-full space-y-6 px-6 py-8 sm:px-12">
+      <Transition mode="out-in">
+        <div class="grid place-content-center">
+          <div class="flex flex-col items-center gap-4 text-center">
+            <Icon
+              name="gnome:sidebar"
+              size="120"
+              class="text-muted-foreground"
+            />
+
+            <p class="text-sm text-muted-foreground">
+              {{ $t("navigate_settingss") }}
+            </p>
+          </div>
+        </div>
+      </Transition>
+    </div>
+  </SettingsContent>
+</template>
+
+<style scoped></style>

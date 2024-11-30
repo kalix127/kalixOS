a30979f66f3a1eb5c7e9677d3f74902c2fdf94a2
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-11-30 21:39:40 +0100

    feat: Disabled kate on mobile

--- components/desktop/app/Kate.vue
+++ components/desktop/app/Kate.vue
@@ -3,6 +3,7 @@ import "@/assets/js/monacoWorker";
 import * as monaco from "monaco-editor";
 import type { HTMLAttributes } from "vue";
 import type { AppNode } from "@/types";
+import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
 
 const props = defineProps<{
   class?: HTMLAttributes["class"];
@@ -15,8 +16,11 @@ const textEditorStore = useTextEditorStore();
 const { openedNode } = storeToRefs(textEditorStore);
 
 let editorObj: monaco.editor.IEditor | undefined;
+const isMobileOrTablet = useBreakpoints(breakpointsTailwind).smaller("lg");
 
 onMounted(() => {
+  if (isMobileOrTablet.value) return;
+
   editorObj = monaco.editor.create(document.getElementById("editor")!, {
     value: openedNode.value?.content || "",
     language: "javascript",
@@ -43,7 +47,15 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div id="editor" />
+  <div v-if="!isMobileOrTablet" id="editor" />
+  <div v-else class="grid h-full w-full place-content-center bg-background p-8">
+    <div class="flex flex-col items-center gap-6">
+      <Icon name="gnome:warning" size="140" class="text-muted-foreground" />
+      <p class="text-center">
+        {{ $t("kate_not_available_on_mobile") }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>

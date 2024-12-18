cc86ea315bd93ded4067601cdaf30f619339cd95
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-12-18 16:16:14 +0100

    remove: Removed useless logic

--- app/composables/useWindowSizes.ts
+++ app/composables/useWindowSizes.ts
@@ -1,57 +1,22 @@
-import {
-  breakpointsTailwind,
-  useBreakpoints,
-  useWindowSize,
-} from "@vueuse/core";
+import { useWindowSize } from "@vueuse/core";
 
 export function useWindowSizes() {
-  const breakpoints = useBreakpoints(breakpointsTailwind);
   const { width: windowWidth } = useWindowSize();
 
-  const getBreakpointSize = () => {
-    if (breakpoints.greater("lg").value)
-      return "lg";
-    return "default";
-  };
-
   const initialWidth = computed(() => {
-    const size = getBreakpointSize();
-    switch (size) {
-      case "lg":
-        return Math.round(windowWidth.value * 0.8);
-      default:
-        return 0;
-    }
+    return Math.round(windowWidth.value * 0.7);
   });
 
   const initialHeight = computed(() => {
-    const size = getBreakpointSize();
-    switch (size) {
-      case "lg":
-        return Math.round((initialWidth.value * 9) / 16);
-      default:
-        return 0;
-    }
+    return Math.round((initialWidth.value * 9) / 16);
   });
 
   const minWidth = computed(() => {
-    const size = getBreakpointSize();
-    switch (size) {
-      case "lg":
-        return 900;
-      default:
-        return 0;
-    }
+    return 900;
   });
 
   const minHeight = computed(() => {
-    const size = getBreakpointSize();
-    switch (size) {
-      case "lg":
-        return Math.round((minWidth.value * 9) / 16);
-      default:
-        return 0;
-    }
+    return Math.round((minWidth.value * 9) / 16);
   });
 
   return {
